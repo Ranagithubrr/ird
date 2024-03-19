b293217065dfe960835082ae8212708567fa6da4
ranagithubrr <mr6430119@gmail.com>
2024-03-20 00:08:36 +0600

    Fix: redirected

--- src/app/page.js
+++ src/app/page.js
@@ -39,18 +39,25 @@ export default function Home() {
   const [activeSubCategoryTitle, setActiveSubCategoryTitle] = useState("The servant is dependent on his Lord");
   console.log(activeSubCategoryTitle)
   return (
-    <SkeletonTheme baseColor="#ffffff" highlightColor="#EBEEF2">      
+    <SkeletonTheme baseColor="#ffffff" highlightColor="#EBEEF2">
+      <div className="bg-green-500 h-screen w-screen fixed flex flex-col z-50 items-center justify-center">
+        <span className="text-white block">This website is relocated at https://ird-beige.vercel.app/</span>
+        <span className="text-white block">To continue Please</span>
+        <a href="https://ird-beige.vercel.app/" className="bg-white text-green-500 font-semibold px-4 py-2 rounded mt-4">
+          Click Here
+        </a>
+      </div>
       <div className="relative grid lg:gap-6 sm-max:overflow-auto xs:flex xs:flex-col xs:gap-0 sm:gap-0 xl:grid-rows-[46px,1fr] 2xl:grid-rows-[46px,1fr] 3xl:grid-rows-[46px,1fr] z-0 xl:grid-cols-[105px,350px,1fr] 2xl:grid-cols-[105px,350px,1fr,270px] 3xl:grid-cols-[105px,350px,1fr,300px] px-2 lg:p-6">
         <Suspense fallback={<div>Loading...</div>}>
           <div className="">
             <Sidebar />
           </div>
           <div>
-            <Categories setActiveSubCategoryTitle={setActiveSubCategoryTitle}/>
+            <Categories setActiveSubCategoryTitle={setActiveSubCategoryTitle} />
           </div>
           <div className="">
             <Navbar />
-            <Dua loading={loading} duaData={duaData} activeSubCategoryTitle={activeSubCategoryTitle}/>
+            <Dua loading={loading} duaData={duaData} activeSubCategoryTitle={activeSubCategoryTitle} />
           </div>
         </Suspense>
       </div>
